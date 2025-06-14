a1298bc149d03a56759741ef232deece22a676d4
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2025-06-14 22:50:21 +0530

    Verify endpoint also updated

--- Trunk/Source/TalkyChat-Api/src/routes/authRoute.js
+++ Trunk/Source/TalkyChat-Api/src/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const{ CreateOtp } = require('../controllers/authController')
+const{ CreateOtp, VerifyOtp } = require('../controllers/authController')
 
 //End-Points 
-router.post('/Create-Otp' ,CreateOtp )
+router.post('/Create-Otp' ,CreateOtp );
+router.post('/Verify-Otp' ,VerifyOtp );
 
 module.exports = router;
