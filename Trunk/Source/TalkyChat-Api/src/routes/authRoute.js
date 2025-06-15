5f5634da338a046f4960670975435e5346236428
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2025-06-15 16:31:25 +0530

    Login Endpoint Implemented

--- Trunk/Source/TalkyChat-Api/src/routes/authRoute.js
+++ Trunk/Source/TalkyChat-Api/src/routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const{ CreateOtp, VerifyOtp, registerUser } = require('../controllers/authController')
+const{ CreateOtp, VerifyOtp, registerUser, loginUser } = require('../controllers/authController')
 
 //End-Points 
 router.post('/Create-Otp' ,CreateOtp );
 router.post('/Verify-Otp' ,VerifyOtp );
 router.post('/Register' ,registerUser );
+router.post('/Login' ,loginUser );
 
 module.exports = router;
