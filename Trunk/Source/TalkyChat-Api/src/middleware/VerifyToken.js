5a524267ed8cb65162526fa2e4fc537f6643caac
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2025-06-19 17:08:14 +0530

    User Profile setup Completed

--- Trunk/Source/TalkyChat-Api/src/middleware/VerifyToken.js
+++ Trunk/Source/TalkyChat-Api/src/middleware/VerifyToken.js
@@ -4,27 +4,37 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ status: 'unauthorized', message: 'Access token missing or invalid' });
+    return res.status(401).json({
+      status: 'unauthorized',
+      message: 'Access token missing or invalid',
+    });
   }
 
   const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = jwtService.verifyAccessToken(token);
+    const decoded = jwtService.verifyToken(token);
 
     if (!decoded) {
-      return res.status(403).json({ status: 'forbidden', message: 'Invalid token' });
+      return res.status(403).json({
+        status: 'forbidden',
+        message: 'Invalid token',
+      });
     }
 
     req.auth = {
-      id: decoded.id,
-      email: decoded.email,
-      username: decoded.username,
+      id: decoded.UserId,
+      email: decoded.Email,
+      username: decoded.User,
     };
+    
 
     next();
   } catch (error) {
-    return res.status(403).json({ status: 'forbidden', message: 'Invalid or expired token' });
+    return res.status(403).json({
+      status: 'forbidden',
+      message: 'Invalid or expired token',
+    });
   }
 };
 
--- Trunk/Source/TalkyChat-Api/src/routes/authRoute.js
+++ Trunk/Source/TalkyChat-Api/src/routes/authRoute.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/VerifyToken');
 
-const{ CreateOtp, VerifyOtp, registerUser, loginUser } = require('../controllers/authController')
+const{ CreateOtp, VerifyOtp, registerUser, loginUser, updateProfile } = require('../controllers/authController')
 
 //End-Points 
 router.post('/Create-Otp' ,CreateOtp );
 router.post('/Verify-Otp' ,VerifyOtp );
 router.post('/Register' ,registerUser );
 router.post('/Login' ,loginUser );
+router.put('/Profile' ,verifyToken, updateProfile);
 
 module.exports = router;
