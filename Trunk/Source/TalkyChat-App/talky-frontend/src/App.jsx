3ad864221a523d0adf99aaff80abd5764f2a5b75
ANIRUDH T ANIL <anirudhtanil85@gmail.com>
2025-07-09 01:17:13 +0530

    Login Page and Dummy Page for Dashboard

--- Trunk/Source/TalkyChat-App/talky-frontend/src/App.jsx
+++ Trunk/Source/TalkyChat-App/talky-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import Home from "./Pages/Home/Home";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
     </Routes>
   );
 }
